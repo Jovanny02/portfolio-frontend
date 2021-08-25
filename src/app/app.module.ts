b8128fa57d8b8fd2f85177ee641ae88250af5631
Jovanny <Jovannyvera@comcast.net>
2021-08-25 17:53:49 +0000

    started working on select-user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SelectUserComponent } from './select-user/select-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectUserComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // the FormsModule is needed for forms
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
